b79c72de76fa1224fa8f3b75b04fa56b37d575bd
Hyewon23 <ch2w2006@kookmin.ac.kr>
2021-02-11 18:25:22 +0900

    [Practice]How to use 'table-component'

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import TicTacToe from "./TicTacToe/TicTacToe";
+import ExampleOne from "./practice/Table/Table1";
+import ExampleTwo from "./practice/Table/Table2";
+import ExampleThree from "./practice/Table/Table3";
+import ExampleFour from "./practice/Table/Table4";
+import ExampleFive from "./practice/Table/Table5";
 
 const App = () => {
     return (
-        <TicTacToe/>
+        <>
+            <ExampleOne/>
+            <ExampleTwo/>
+            <ExampleThree/>
+            <ExampleFour/>
+            <ExampleFive/>
+        </>
     );
 };
 
