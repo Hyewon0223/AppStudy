1b5a16f94f646ece3dac492e3799ef00d48600d4
Hyewon23 <ch2w2006@kookmin.ac.kr>
2021-02-22 00:31:31 +0900

    [Weather]connect API

--- Weather/Weather.js
+++ Weather/Weather.js
@@ -2,22 +2,15 @@ import React, {useState} from 'react';
 import {SafeAreaView, StyleSheet, View, Text, Button, TouchableOpacity} from 'react-native';
 import AppLoading from 'expo-app-loading';
 import {useFonts} from 'expo-font';
-import * as axios from "react-native";
+import axios from 'axios';
 
 export const Weather = async() => {
     const apiKey =process.env.REACT_APP_OPENWEATHER_KEY;
     const URL = `http://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=${apiKey}`;
 
-    console.log(URL);
-
-    fetch(URL).then(result => {
-        // result = result.data;
-        console.log(result);
-        console.log('Seoul',result.response);
+    axios.get(URL).then(response => {
+        console.log(response.data);
     });
-
-    // const data = await axios.get(URL);
-    // console.log(data);
 
     return <>
         <Text>Weather</Text>
