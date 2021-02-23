a3d6a2176ffde3d61a70a46a69f83a0d0a9f6e8d
Hyewon23 <ch2w2006@kookmin.ac.kr>
2021-02-23 20:04:19 +0900

    [Weather]basic

--- Weather/Weather.js
+++ Weather/Weather.js
@@ -1,20 +1,68 @@
 import React, {useState} from 'react';
 import {SafeAreaView, StyleSheet, View, Text, Button, TouchableOpacity} from 'react-native';
 import AppLoading from 'expo-app-loading';
+import React, {useState,useEffect} from 'react';
+import {SafeAreaView, StyleSheet, View, Text, Button, TouchableOpacity} from 'react-native';
+import AppLoading from 'expo-app-loading';
 import {useFonts} from 'expo-font';
 import axios from 'axios';
 
-export const Weather = async() => {
-    const apiKey =process.env.REACT_APP_OPENWEATHER_KEY;
-    const URL = `http://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=${apiKey}`;
+export const Weather = () => {
+    const [select, setSelect] = useState({
+        value: 'Seoul',
+    })
 
-    axios.get(URL).then(response => {
-        console.log(response.data);
+    const getValue = e => {
+        const {name, value} = e.target;
+        setSelect({
+            ...select,
+            [name]: value,
+        })
+    };
+
+    const [weather, setWeather] = useState({
+        loc: 'Seoul',
+        country: 'KR',
+        temp: 0,
+        desc: '',
+        icon: '',
     });
+
+    const PressEnter = e => {
+        if (e.key === 'Enter') searchWeather();
+    }
+
+    const searchWeather = () => {
+        const apiKey = process.env.REACT_APP_OPENWEATHER_KEY;
+        const URL = `http://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=${apiKey}`;
+
+        axios.get(URL).then(response => {
+            response = response.data;
+            console.log(response);
+            console.log(response.weather[0].description);
+
+            setWeather({
+                loc: response.name,
+                country: response.sys.country,
+                temp: Math.round(response.main.temp - 273.15),
+                desc: response.weather[0].description,
+                icon: `http://openweathermap.com/img/w/${response.weather[0].icon}.png`,
+            })
+
+            console.log('loc : ', weather.loc);
+            console.log('country : ', weather.country);
+            console.log('temp : ', weather.temp);
+            console.log('desc : ', weather.desc);
+            console.log('icon : ', weather.icon);
+
+        });
+    }
+
+    useEffect(() => {
+        searchWeather();
+    }, []);
 
     return <>
         <Text>Weather</Text>
     </>
 }
-
-export default Weather;
