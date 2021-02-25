341fa00db375002581fba5408f6a5c5d90a247d1
Hyewon23 <ch2w2006@kookmin.ac.kr>
2021-02-26 02:10:40 +0900

    [Weather]practice <Image> & complete mainWeather

--- Weather/Weather.js
+++ Weather/Weather.js
@@ -1,34 +1,32 @@
 import React, {useState,useEffect} from 'react';
-import {SafeAreaView, StyleSheet, View, Text, Button, TouchableOpacity} from 'react-native';
+import {SafeAreaView, StyleSheet, View, Text, Button, TouchableOpacity, TextInput,Image, TouchableHighlight} from 'react-native';
 import AppLoading from 'expo-app-loading';
 import {useFonts} from 'expo-font';
 import {OPENWEATHER_KEY} from '@env';
 import axios from "axios";
 
-export const Weather = () => {
-    const [select, setSelect] = useState({
-        value: 'Seoul',
-    })
+import close from './img/close.png';
+import search from './img/search.png';
+import thermometer from './img/thermometer.png';
+import wind from './img/wind.png';
+import {ImageBackground} from "react-native-web";
+import * as url from "url";
 
-    const getValue = e => {
-        const {name, value} = e.target;
-        setSelect({
-            ...select,
-            [name]: value,
-        })
-    };
+
+
+export const Weather = () => {
+    const [year, month, date] = [new Date().getFullYear(), new Date().getMonth()+1,new Date().getDate()];
+    const today = `${year}년 ${month}월 ${date}일`
+
+    const [search, setSearch] = useState('Seoul');
 
     const [weather, setWeather] = useState({
         loc: 'Seoul',
         country: 'KR',
         temp: 0,
-        desc: '',
-        icon: '',
+        desc: '-',
+        icon: '-',
     });
-
-    const PressEnter = e => {
-        if (e.key === 'Enter') searchWeather();
-    }
 
     const searchWeather = () => {
         const apiKey = OPENWEATHER_KEY;
@@ -37,17 +35,23 @@ export const Weather = () => {
         axios.get(URL).then(response => {
             response = response.data;
             console.log(response);
-            // console.log(response.weather[0].description);
 
             setWeather({
                 loc: response.name,
                 country: response.sys.country,
-                temp: Math.round(response.main.temp - 273.15),
+                // temp: Math.round(response.main.temp - 273.15),
+                temp: response.main.temp,
                 desc: response.weather[0].description,
                 icon: `http://openweathermap.com/img/w/${response.weather[0].icon}.png`,
             })
-            console.log(weather);
         });
+        return weather;
+    }
+
+    const pressDel = () => {
+        setWeather({
+            loc : '',
+        })
     }
 
     useEffect(() => {
@@ -56,9 +60,45 @@ export const Weather = () => {
 
     return <>
         <SafeAreaView>
-            <Text>Weather</Text>
-        </SafeAreaView>
+            <View style={styles.weatherInput}>
+                <TextInput placeholder={"지역을 입력해주세요"} onChangeText={value => setWeather({loc : value})} defaultValue={weather.loc} />
+                <Image style={{height:30,width:30,resizeMode:'contain'}} onClick={() => {searchWeather()}} source={require('./img/search.png')}/>
+                </View>
+
+            <View style={styles.mainWeather}>
+                <Text>{today}</Text>
+                <Text>{weather.loc}, {weather.country}</Text>
+                <Image style={{height:'100%',width:'100%',resizeMode:'contain'}} source={{uri: weather.icon}}/>
+                <Text>{weather.desc}</Text>
+                <Text>{weather.temp}</Text>
+            </View>
+
+            <View style={styles.temperatures}>
+
+            </View>
+
+            <View style={styles.wind}>
+
+            </View>
+            </SafeAreaView>
     </>
 }
 
 export default Weather;
+
+const styles = StyleSheet.create({
+    weatherInput : {
+        flexDirection:"row",
+        alignItems:'center',
+        // justifyContent: 'space-between',
+    },
+    mainWeather : {
+
+    },
+    temperatures : {
+
+    },
+    wind : {
+
+    },
+})
